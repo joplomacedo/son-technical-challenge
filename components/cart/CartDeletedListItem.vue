cb4dae827d8d6d6b75ef8517a70bfe9814ffe1a2
joplomacedo <joplomacedo@gmail.com>
2024-07-18 00:53:48 +0100

    updates: CartDeletesListItem styles

--- components/cart/CartDeletedListItem.vue
+++ components/cart/CartDeletedListItem.vue
@@ -1,78 +1,92 @@
 <template>
 	<li :class="$style.root">
-		<div :class="$style.imageBox">
+		<div :class="$style.main">
 			<NuxtImg
+				:class="$style.image"
 				:src="item.product.thumbnail"
 				:alt="item.product.name"
-				:class="$style.image"
 				:width="imgSize"
 				:height="imgSize"
 				:placeholder="[imgSize, imgSize, 75, 5]"
 				:placeholder-class="$style['is-placeholder']"
 				fit="cover"
 			/>
-		</div>
+			<div class="flex-grow flex flex-col justify-between py-2">
+				<div class="flex justify-between items-start">
+					<div>
+						<p :class="$style.name">{{ item.product.name }}</p>
+						<p :class="$style.price">{{ $formatPrice(item.product.price) }}</p>
+					</div>
 
-		<div :class="$style.content">
-			<div>
-				<p :class="$style.name">{{ item.product.name }}</p>
-				<p :class="$style.price">{{ $formatPrice(item.product.price) }}</p>
+					<p :class="$style.priceTotal">{{ $formatPrice(item.total) }}</p>
+				</div>
 
-				<div :class="$style.quantityAndActions">
-					<p :class="$style.quantity">x {{ item.quantity }}</p>
+				<div class="flex justify-between gap-2">
+					<div class="flex gap-4 items-center">
+						<span>
+							{{ item.quantity }}
+						</span>
+
+						<BaseSpinner
+							size="sm"
+							v-if="addItemStatus === 'pending'"
+						/>
+					</div>
+
+					<BaseButton
+						:class="$style.stepper__btn"
+						size="xs"
+						@click="() => handleAddToCart(item.quantity)"
+						:disabled="isCartBusy"
+					>
+						Add to cart
+					</BaseButton>
 				</div>
 			</div>
-
-			<BaseButton
-				@click="() => addItem(item.quantity)"
-				:disabled="isCartBusy"
-				:class="$style.restoreBtn"
-				size="xs"
-				:loading="addItemStatus === 'pending'"
-			>
-				Add to cart
-			</BaseButton>
 		</div>
 	</li>
 </template>
 
 <script setup lang="ts">
-import { useAddItemMutation } from "@/queries/cart";
+/* TODO: figure best way to share code with CartListItem */
 import type { CartItem } from "@/queries/cart";
-
-const { isCartBusy } = useCartIsBusy();
-
+import { useAddItemMutation } from "~/queries/cart";
 const props = defineProps<{
 	item: CartItem;
 }>();
+
+const imgSize = 100;
+const imgSizeWithUnit = `${imgSize}px`;
 
 const { mutate: addItem, status: addItemStatus } = useAddItemMutation(
 	props.item.id
 );
 
-const imgSize = 100;
-const imgSizeWithUnit = `${imgSize}px`;
+const { isCartBusy } = useCartIsBusy();
+
+const handleAddToCart = (quantity: number) => {
+	addItem({ quantity, safe: true });
+};
 </script>
 
 <style module>
-.root {
+.main {
 	display: flex;
 	gap: theme("spacing.4");
 }
 
-.imageBox {
-	--size: v-bind(imgSizeWithUnit);
-	width: var(--size);
-	height: var(--size);
-
-	border-radius: theme("borderRadius.md");
-	background-color: theme("colors.gray.100");
-	overflow: hidden;
-	flex-shrink: 0;
-	padding: theme("spacing.4");
+.name {
+	font-weight: theme("fontWeight.semibold");
 }
 
 .image {
+	--size-img: v-bind(imgSizeWithUnit);
+	width: var(--size-img);
+	height: var(--size-img);
+	border-radius: theme("borderRadius.md");
+	background-color: theme("colors.gray.100");
+	padding: theme("spacing.4");
+	flex-shrink: 0;
 	transition: filter 0.3s;
 
 	&.is-placeholder {
@@ -82,10 +96,6 @@ const imgSizeWithUnit = `${imgSize}px`;
 
 .content {
 	padding-block: theme("spacing.2");
-	display: flex;
-	justify-content: space-between;
-	flex-grow: 1;
-	flex-wrap: wrap;
 }
 
 .name {
@@ -93,9 +103,16 @@ const imgSizeWithUnit = `${imgSize}px`;
 }
 
 .price {
+	color: theme("colors.gray.500");
 }
 
-.restoreBtn {
-	align-self: flex-start;
+.priceTotal {
+	font-weight: theme("fontWeight.semibold");
+}
+
+@media screen and (max-width: 640px) {
+	.image {
+		--size-img: 75px;
+	}
 }
 </style>
